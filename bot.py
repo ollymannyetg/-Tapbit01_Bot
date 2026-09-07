333577db87b2e75b47b578a264a1880585e81c7c
ollymannyetg <olaoluwaadekunle12@gmail.com>
2026-09-07 14:45:08 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,33 @@ from telegram.constants import ChatMemberStatus
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+coin_aliases = {
+    "BITCOIN": "BTC",
+    "ETHEREUM": "ETH",
+    "SOLANA": "SOL",
+    "BINANCECOIN": "BNB",
+    "RIPPLE": "XRP",
+    "DOGECOIN": "DOGE",
+    "PEPE": "PEPE",
+    "ZCASH": "ZEC",
+    "ETHEREUMCLASSIC": "ETC",
+    "CARDANO": "ADA",
+    "TRON": "TRX",
+    "AVALANCHE": "AVAX",
+    "POLKADOT": "DOT",
+    "CHAINLINK": "LINK",
+    "LITECOIN": "LTC",
+    "BITCOINCASH": "BCH",
+    "COSMOS": "ATOM",
+    "UNISWAP": "UNI",
+    "AAVE": "AAVE",
+    "NEAR": "NEAR",
+    "APTOS": "APT",
+    "ARBITRUM": "ARB",
+    "OPTIMISM": "OP",
+    "POLYGON": "POL"
+}
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message:
         return
@@ -440,33 +467,6 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     coin = clean_text
     coin = coin_aliases.get(coin, coin)
-
-    coin_aliases = {
-        "BITCOIN": "BTC",
-        "ETHEREUM": "ETH",
-        "SOLANA": "SOL",
-        "BINANCECOIN": "BNB",
-        "RIPPLE": "XRP",
-        "DOGECOIN": "DOGE",
-        "PEPE": "PEPE",
-        "ZCASH": "ZEC",
-        "ETHEREUMCLASSIC": "ETC",
-        "CARDANO": "ADA",
-        "TRON": "TRX",
-        "AVALANCHE": "AVAX",
-        "POLKADOT": "DOT",
-        "CHAINLINK": "LINK",
-        "LITECOIN": "LTC",
-        "BITCOINCASH": "BCH",
-        "COSMOS": "ATOM",
-        "UNISWAP": "UNI",
-        "AAVE": "AAVE",
-        "NEAR": "NEAR",
-        "APTOS": "APT",
-        "ARBITRUM": "ARB",
-        "OPTIMISM": "OP",
-        "POLYGON": "POL"
-    }
 
     coin_ids = {
         "BTC": "bitcoin",
